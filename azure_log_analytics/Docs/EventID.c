ba47c52d25310feea9eac3be1fac0c537f2aefd1
Karol <93782725+petitess@users.noreply.github.com>
2022-03-18 16:14:56 +0100

    Create EventID.c

---
+++ azure_log_analytics/Docs/EventID.c
@@ -0,0 +1,29 @@
+1000 app error
+1002 app hang
+4719 System audit policy was changed
+4648 account login with expliciti credentials
+4735 security-enabled group modification
+4624 login success
+4625 login fail
+4771 Failed logon attempts
+4740 Locked Accounts
+624 User Account Created
+4720 User Account Created
+630 User Account Deleted
+4726 User Account Deleted
+631 Groups Created
+4727 Groups Created
+634 Group Deleted
+4730 Group Deleted
+4728 Group Membership Changes 
+4729 Group Membership Changes
+4732 Group Membership Changes
+4733 Group Membership Changes
+4756 Group Membership Changes 
+4757 Group Membership Changes
+632 Group Membership Changes
+633 Group Membership Changes 
+636 Group Membership Changes
+637 Group Membership Changes
+660 Group Membership Changes
+661 Group Membership Changes
